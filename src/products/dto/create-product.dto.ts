5f2d359765f987b71f2bbaf0585fcbbe10c1bb4f
Wilmar dos Santos <dswilmar@gmail.com>
2023-05-23 21:26:15 -0300

    feat: validações

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,5 +1,18 @@
+import { IsNotEmpty, IsNumber, IsString, MaxLength, Min } from "class-validator";
+
 export class CreateProductDto {
+    @MaxLength(255)
+    @IsString()
+    @IsNotEmpty()
     name: string;
+
+    @Min(0)
+    @IsNumber()
+    @IsNotEmpty()
     price: number;
+
+    @Min(1)
+    @IsNumber()
+    @IsNotEmpty()
     categoryId: number;
 }
